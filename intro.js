3e8f85c9b9daacefb71e52f112516dcbb945c8bd
J <167507808+LuminaryPulse@users.noreply.github.com>
2026-04-10 16:08:15 +0300

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Magreth";
+const age = 18;
+const favouriteLanguage = "Python";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favourite Programming Language:", favouriteLanguage);
